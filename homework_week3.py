276aeeee199c34c64c4d18c2060a19884e671e37
Yongwoo-Seok <cyan0320@kaist.ac.kr>
2019-11-04 22:03:57 +0900

    숙제3
    숙제 3 테스트

---
+++ homework_week3.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.Sparta_homework
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908', headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 음악 제목
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(2) > td.info > a.title.ellipsis
+
+# 가수 이름
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+
+# 앨범 이름
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.albumtitle.ellipsis
+
+# 앨범 그림
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > a > span
+
+# 노래 순위
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
+
+# 음악 차트 크롤링
+musics = soup.select('body-content > div.newest-list > div > table > tbody > tr')
+for music in musics:
+    name = music.select_one('td.info > a.title.ellipsis')
+    rank = music.select_one('td.number')
+    artist = music.select_one('td.info > a.artist.ellipsis')
+    album = music.select_one('td.info > a.albumtitle.ellipsis')
+    #album_image = music.select_one('td:nth-child(3) > a > span')
+    data = {
+        'rank': rank.text,
+        'name': name.text,
+        'artist': artist.text,
+        'album': album.text
+    }
+    db.music_rank.insert_one(data)
